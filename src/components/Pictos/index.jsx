03b3ef94d272923f5b1e31d523d5ec17be7bfb43
Anthony Fouda Many <anthony.foudamany@gmail.com>
2023-10-04 10:46:22 +0200

    ⚡ Amélioration composant : Ajout des props text et class animation

--- src/components/Pictos/index.jsx
+++ src/components/Pictos/index.jsx
@@ -1,7 +1,10 @@
 import './index.css'
 
-export default function Pictos({link, img}){
-    return <a href={link} className="pictos-link" target='_blank' rel='noreferrer'> 
-        <img src={img} alt="imgName" className='pictos-img' />
-    </a>
+export default function Pictos({link, img, animation, text}){
+    return <div className='picto-container'>
+            <p className={ text ? "pictos-text" : ""}>{text ?? text}</p>
+            <a href={link} className={animation ? "pictos-link pictos-animation" : "pictos-link"} target='_blank' rel='noreferrer'> 
+                <img src={img} alt="imgName" className={animation ? 'pictos-img animation-img' : 'pictos-img'}/>
+            </a>
+        </div>
 }
