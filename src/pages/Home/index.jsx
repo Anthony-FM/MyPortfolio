f83efb92f45ad2cbbafc5af7b53dd929ece47cb8
Anthony Fouda Many <anthony.foudamany@gmail.com>
2023-10-04 10:42:49 +0200

    ✨ Ajouts composants pages portfolio et contact

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,11 +6,13 @@ import Loading from "../../components/Loading"
 
 //Components Pages
 import HomePage from "../../components/HomePage"
-import About from "../../components/About"
+import About from "../../components/AboutPage"
+import PortefolioPage from "../../components/PortfolioPage"
+import Contact from "../../components/ContactPage"
 
 export default function Home(){
     const { datas, isLoading, error} = useFetch("../data.json")
-
+    
     return <> {
         isLoading ? <Loading/>
         : error ? <p>{error}</p> :
@@ -24,6 +26,8 @@ export default function Home(){
             <About 
                 APropos={datas.APropos}
              />
+            <PortefolioPage datasProject={datas.Projects}/>
+            <Contact/>
         </div>
     }
     
