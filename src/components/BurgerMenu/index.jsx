59396efec0ee9d922b7b9fd2c3dac7e0f1327a2f
Anthony Fouda Many <anthony.foudamany@gmail.com>
2023-10-04 10:51:24 +0200

    ⚡ Amélioration composant :  🎨 màj style et amélioration animation

--- src/components/BurgerMenu/index.jsx
+++ src/components/BurgerMenu/index.jsx
@@ -8,10 +8,11 @@ import { addisBurgerOpen } from '../../feature/myPortefolioFeatures'
 export default function BurgerMenu(){
     const dispatch = useDispatch()
     const isBurgerOpen = useSelector(selectMyslice).isBurgerOpen
-    return <div className="burgerMenu-container" onClick={() => dispatch(addisBurgerOpen())}>
+    const DarkMode = useSelector(selectMyslice).darkMode
+    return <div className={ isBurgerOpen ? "burgerMenu-container burger-left" : "burgerMenu-container"} onClick={() => dispatch(addisBurgerOpen())}>
 
-            <div className={isBurgerOpen ? "burger-top bun-move-down" : "burger-top"}></div>
-            <div className={isBurgerOpen ? "" : "burger-middle"}></div>
-            <div className={isBurgerOpen ? "burger-bottom bun-move-up" : "burger-bottom"}></div>
+            <div className={DarkMode ? (isBurgerOpen ? "burger-top backgroundColor-white bun-move-down" : "burger-top backgroundColor-white") : (isBurgerOpen ? "burger-top backgroundColor-blue bun-move-down" : "burger-top backgroundColor-blue")}></div>
+            <div className={DarkMode ? (isBurgerOpen ? "" : "burger-middle backgroundColor-white") : (isBurgerOpen ? "" : "burger-middle backgroundColor-blue")}></div>
+            <div className={DarkMode ? (isBurgerOpen ? "burger-bottom backgroundColor-white bun-move-up" : "burger-bottom backgroundColor-white") : (isBurgerOpen ? "burger-bottom backgroundColor-blue bun-move-up" : "burger-bottom backgroundColor-blue")}></div>
         </div>
 }
