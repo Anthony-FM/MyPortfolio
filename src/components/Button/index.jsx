ae2cf8c19898d612505b4ad79804e9a38fa2d6b3
Anthony Fouda Many <anthony.foudamany@gmail.com>
2023-10-04 10:52:12 +0200

    ⚡ Amélioration composant : 🎨 styles

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -5,7 +5,7 @@ import { selectMyslice } from '../../utils/redux/selector'
 
 export default function Button({text, img, imgName, link, download, target}){
     const darkMode = useSelector(selectMyslice).darkMode
-    return <button  className={darkMode ? 'button backgroundColor-blue border-lightblue' : 'button backgroundColor-white border-lightblue'}>
+    return <button  className={darkMode ? 'button backgroundColor-blue border-lightBlue' : 'button backgroundColor-white border-lightBlue'}>
         <a href={link} target={target}>
             <p className={ darkMode ? "button-text color-white" : "button-text color-blue"}>{text}</p>
             <img src={img} alt={imgName} className="button-picto" download={download} />
