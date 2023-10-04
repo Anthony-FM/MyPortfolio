fa2e236c9d9eaf342118eadaf6fbbcd7cfefab72
Anthony Fouda Many <anthony.foudamany@gmail.com>
2023-10-04 10:49:57 +0200

    ⚡ Amélioration composant : 🎨 styles et animation

--- src/components/LogoSkills/index.jsx
+++ src/components/LogoSkills/index.jsx
@@ -7,7 +7,10 @@ import { selectMyslice } from "../../utils/redux/selector"
 export default function LogoSkills({src, alt}){
     const DarkMode = useSelector(selectMyslice).darkMode
 
-    return <div className="skill-container">
+    return <div className="skill-container" >
+        <div className="skill-containerImg">
             <img src={src} alt={alt} />
+            <h3 className={DarkMode ? "skill color-white" : "skill"}>{alt}</h3>
+        </div>
         </div>
 }
