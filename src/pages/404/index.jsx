8037e39bfa96228e94d60d72cf8e8e095b0ade7b
Anthony Fouda Many <anthony.foudamany@gmail.com>
2023-10-05 18:55:35 +0200

    🎨 Amélioration design page 404

--- src/pages/404/index.jsx
+++ src/pages/404/index.jsx
@@ -1,5 +1,14 @@
+import './index.css'
+// hook
+import { useSelector } from 'react-redux'
+// Slice
+import { selectMyslice } from '../../utils/redux/selector'
+
 export default function Error404(){
-    return <>
-    404
-    </>
+    const darkMode = useSelector(selectMyslice).darkMode
+
+    return <section className={darkMode ? ("error-container backgroundColor-black") : ("error-container")}>
+        <h1 className={darkMode ? ("error-title color-white") : ("error-title")}>Erreur 404</h1>
+        <p className={ darkMode ? ("error-text color-white") : ("error-text")}>Désolé mauvais endroit...Oops!</p>
+    </section>
 }
