83d655cb168d327989722bb857b9ef0e80c4eb74
Anthony Fouda Many <anthony.foudamany@gmail.com>
2023-10-14 14:47:06 +0200

    :fire: Suppression Redux ( DarkMode ) et des assets non utilisés. Ajout anim 🎥

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,34 +1,29 @@
 // Css
 import './index.css'
 
-// Redux
-import { useSelector } from 'react-redux'
-import { selectMyslice } from '../../utils/redux/selector'
+
 // Assets
-import GithubBlack from '../../assets/skills/github-black.svg'
 import GithubWhite from '../../assets/github-white.svg'
-import Linkedin from '../../assets/linkedin-black.png'
 import LinkedinWhite from '../../assets/linkedin-white.svg'
 // Composants
-import Pictos from '../Pictos'
+import PictosFooter from '../PictosFooter'
 
 export default function Footer(){
-    const darkMode = useSelector(selectMyslice).darkMode
     return <footer className="backgroundColor-black">
         <div className="footer-text color-white">
             Portfolio réalisé avec ❤️ par <br/> Anthony Fouda-Many | 2023
              
         </div>
         <div className="footer-pictosContainer">
-            <Pictos 
+            <PictosFooter 
                 link="https://github.com/Anthony-FM"
-                img={darkMode ? GithubBlack : GithubWhite }
+                img={GithubWhite }
                 imgName="github link"
                 animation='false'
             />
-            <Pictos 
+            <PictosFooter
                 link="https://www.linkedin.com/in/anthony-fouda-many/"
-                img={darkMode ? Linkedin : LinkedinWhite}
+                img={LinkedinWhite}
                 imgName="linkedin link"
                 animation='false'
             />
