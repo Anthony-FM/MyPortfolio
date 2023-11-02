863ef6d10fd24cd6d58ab323ecbdecb40e1e7917
Anthony Fouda Many <anthony.foudamany@gmail.com>
2023-11-02 22:05:24 +0100

    🔥 Modif trie

--- src/components/Sort/index.jsx
+++ src/components/Sort/index.jsx
@@ -5,15 +5,15 @@ import { useSelector, useDispatch } from 'react-redux'
 // Slice
 import { selectMyslice } from '../../utils/redux/selector';
 // Action
-import { addProjects } from '../../feature/myPortefolioFeatures';
+import { addOpenclassroomsProjects } from '../../feature/myPortefolioFeatures';
 // components
 import Options from '../Options';
 
-export default function Sort({word}){
+export default function Sort({index}){
     const dispatch = useDispatch()
-    const datas = useSelector(selectMyslice).datas
+    const projectsArray = useSelector(selectMyslice).datas.Projects[index].studyProjects
     const darkMode = useSelector(selectMyslice).darkMode
-    const keywords = datas.Projects.studyProjects.map((project) => project.keywords)
+    const keywords = projectsArray.map((project) => project.keywords)
     let keyword = []
     keywords.forEach(key => {
         keyword = keyword.concat(key)
@@ -22,25 +22,26 @@ export default function Sort({word}){
 
     function sortByWords(word){
         if(word === "Tous les projets"){
-            dispatch(addProjects(datas.Projects.studyProjects))
+            dispatch(addOpenclassroomsProjects(projectsArray))
             return
-        }
-        const newData = datas.Projects.studyProjects.filter((project) => project.keywords.includes(word))
-        dispatch(addProjects(newData))
+        }        
+        const openclassroomsProjects = projectsArray.filter((projects) => projects.keywords.includes(word))
+        dispatch(addOpenclassroomsProjects(openclassroomsProjects))
         return
     }
 
     return <div className="select-container">
         <label htmlFor="trie" className={darkMode ? 'color-white' : "color-blue"}> Projets utilisants: </label>
         <select name="technique" id="trie" onChange={(e) => sortByWords(e.target.value)}>
-            <option value="Tous les projets" >Tous les projets</option>
+            <option value="Tous les projets" defaultValue="React">Tous les projets</option>
             {uniqueKeywords ? uniqueKeywords.map((keyword, index) => {
                 return <Options 
                     value={keyword}                    
                     index={index*10}
                     key={index}
                     />
-            }) : ""
+                }) 
+                : ""
             }
 
         </select>
