60b8a50ec8faa4d1ee6f3f09a02e8d356e5b2068
Anthony Fouda Many <anthony.foudamany@gmail.com>
2023-11-10 14:49:48 +0100

    🔥 suppression fonction tick

--- src/components/HomePage/index.jsx
+++ src/components/HomePage/index.jsx
@@ -10,32 +10,23 @@ import Github from '../../assets/skills/github.svg'
 import GithubBlack from '../../assets/skills/github-black.svg'
 import Linkedin from '../../assets/linkedin-black.png'
 import LinkedinBlue from '../../assets/linkedin-blue.png'
-// Action
-import { addChangeStateOfisDeleting, 
-    addDelta, 
-    addTitleText, 
-    addRemoveLoopNumber, } from '../../feature/myPortefolioFeatures';
+// Action Observer
 import { addObserverHome } from '../../feature/myIntersectionObserver'
-// 
+// Hook
 import { useSelector, useDispatch } from 'react-redux'
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef} from 'react';
 // Selector
 import { selectMyslice, selectObserver } from '../../utils/redux/selector';
 // Components
 import Button from '../Button';
 import Pictos from '../Pictos';
+import WorkTitle from '../WorkTitle'
 
 
-export default function HomePage({titles, presentation, name}){
+
+export default function HomePage ({presentation, name}) {
     // dispatch
-    const dispatch = useDispatch()
-    // data
-    const mainTitle = useSelector(selectMyslice).titleText
-    const isDeleting = useSelector(selectMyslice).isDeleting
-    const loopNumber = useSelector(selectMyslice).loopNumberOfIndexArray
-    const period = useSelector(selectMyslice).period
-    const delta = useSelector(selectMyslice).deltaPeriod
-    const titlesArray = titles
+    const dispatch = useDispatch()    
     // DarkMode
     const darkMode = useSelector(selectMyslice).darkMode
     // Intersection Observer
@@ -43,7 +34,7 @@ export default function HomePage({titles, presentation, name}){
     const homeIsVisible = useSelector(selectObserver).homeIsVisible
     // Ratio
     const ratio = 0.1
-    const y = Math.round(window.innerHeight * ratio)    
+    const y = Math.round(window.innerHeight * ratio)   
 
     useEffect(() => {
         const observer = new IntersectionObserver((entries) => {
@@ -56,46 +47,6 @@ export default function HomePage({titles, presentation, name}){
         })
         observer.observe(homeRef.current)
     }, [dispatch, y])
-
-
-
-    useEffect(() => {
-        function tick(){
-            let i = loopNumber % titlesArray.length;
-            let fullText = titlesArray[i]
-            let upDatedText = isDeleting ? fullText.substring(0, mainTitle.length - 1) : fullText.substring(0, mainTitle.length + 1)
-    
-            dispatch(addTitleText(upDatedText));
-            
-            //
-            if (isDeleting) {
-                dispatch(prevDelta => prevDelta /2)
-            }
-            // Dès que le texte est entier, on fait une pause avant de supprimer
-            if(!isDeleting && upDatedText === fullText) {
-                // Pause avant de supprimer le texte              
-
-                setTimeout(() => {
-                    
-                    dispatch(addChangeStateOfisDeleting(true))
-                    dispatch(addDelta(period))
-
-                }, 800)
-            // Pendant le suppression du texte, dès que la taille du mot arrive à un,
-            // on change l'état, on change de mot et de periode
-            } else if(isDeleting && upDatedText.length === 1) {
-                dispatch(addChangeStateOfisDeleting(false))
-                dispatch(addRemoveLoopNumber(loopNumber + 1))
-                dispatch(addDelta(100))
-            }
-        }
-        let ticker = setInterval(() => {
-            tick();
-        }, delta)
-
-        return () => { clearInterval(ticker)}
-    },[delta, dispatch, isDeleting, loopNumber, mainTitle, period, titlesArray])
-
     
     
     return <section 
@@ -114,7 +65,7 @@ export default function HomePage({titles, presentation, name}){
                 <p className='home-first'>Hello <img src={Hello} className='home-img-hand' alt='hand saying hello'/> je m'appelle:</p>
                 <h1 className={ darkMode ? "home-name color-white" : "home-name"}>{name}</h1>
                 <h2 className="home-works"> Développeur d'application <br/>Javascript - React </h2> 
-                <p className={darkMode ? "citation color-grey-light" : "citation color-lightBlue"}>{mainTitle}<span className={darkMode ? "barre backgroundColor-white" : "barre backgroundColor-black"}></span></p>
+                <WorkTitle />
                 <p className={darkMode ? "home-paragraphe color-white" : "home-paragraphe"}>{presentation} </p>
                 <Button text="Mon CV" link={CV} img={darkMode ? BoxArrowWhite : BoxArrow} imgName="pdf-picto" target="_blank"/>
 
