c532a5f300b42c770b5f18900f1e3c5b7ad2f875
Anthony Fouda Many <anthony.foudamany@gmail.com>
2024-10-01 06:50:10 +0200

    MàJ cv

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './utils/style/index.css';
-import { HashRouter as Router, Route,Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
 
 // Pages
 import Home from './pages/Home'
